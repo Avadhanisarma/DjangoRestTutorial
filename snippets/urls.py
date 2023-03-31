91213a36a6d0de9579b36ff355f43e08c5ae1a4b
Avadhani <avadhanicse@gmail.com>
2023-03-31 14:43:11 +0530

    initial commit

---
+++ snippets/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path,include
+from snippets import views
+from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+
+
+
+router = DefaultRouter()
+router.register(r'snippets', views.SnippetViewSet,basename="snippet")
+router.register(r'users', views.UserViewSet,basename="user")
+
+# The API URLs are now determined automatically by the router.
+urlpatterns = [
+    path('', include(router.urls)),
+]
